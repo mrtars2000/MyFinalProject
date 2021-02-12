1218444f44c98ffed024edd1ecafc7ca590669d3
murat <mrtars2000@gmail.com>
2021-02-12 15:35:15 +0300

    Proje dosyası ekle.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    /// <summary>
+    /// IEntity implement eden bir class veri tabanı tablosudur
+    /// </summary>
+    public interface IEntity
+    {
+    }
+}
