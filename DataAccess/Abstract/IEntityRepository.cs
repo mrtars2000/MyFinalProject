084e3aabaebf4d513d812d513269d5d51f7b5ce6
murat <mrtars2000@gmail.com>
2021-02-13 23:30:58 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    /// <summary>
+    /// Expression =  (p=>p.CategoryId==2)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// 
+    //Generic constrait
+    //class : referans tip
+    //IEntity olabilir yada IEntity den implemente olan bir class olabilir
+    //new() : newlenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter = null);
+        T Get(Expression<Func<T,bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
